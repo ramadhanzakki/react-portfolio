d30d27eb9e2b2e9fcf2b0c8fdc97fb6c92c78037
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-07-31 23:03:51 +0700

    created hero image profile

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -29,26 +29,28 @@ const Hero = () => {
                 </div>
             </div>
         <div>
-            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                    <linearGradient
-                        id="heroGradient"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stopColor="#C2410C" />
-                        <stop offset="100%" stopColor="#F59E0B" />
-                    </linearGradient>
-                </defs>
-                <path
-                    fill="url(#heroGradient)"
-                    d="M53.2,-63.9C68.1,-51,78.7,-33.4,82.5,-14.2C86.2,5,83.3,25.7,73.6,42.5C63.9,59.4,47.5,72.4,28.8,79.2C10.1,86,-10.8,86.7,-29.3,80.1C-47.7,73.5,-63.7,59.7,-73.6,42.5C-83.5,25.3,-87.4,4.6,-84.2,-15.1C-81.1,-34.8,-70.9,-53.6,-55.7,-66.4C-40.4,-79.2,-20.2,-86.1,-0.5,-85.4C19.2,-84.8,38.4,-76.7,53.2,-63.9Z"
-                    transform="translate(100 100)"
-                />
-            </svg>
-            
+            <img src="/public/img/IMG_20260505_092432-removebg-preview.png" alt="photo profile" className="max-w-full mx-auto"/>
+            <span className="absolute bottom-0 -z-10 left-1/2 -translate-x-1/2 translate-y-20">
+                <svg viewBox="0 0 200 200" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <linearGradient
+                            id="heroGradient"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stopColor="#C2410C" />
+                            <stop offset="100%" stopColor="#F59E0B" />
+                        </linearGradient>
+                    </defs>
+                    <path
+                        fill="url(#heroGradient)"
+                        d="M53.2,-63.9C68.1,-51,78.7,-33.4,82.5,-14.2C86.2,5,83.3,25.7,73.6,42.5C63.9,59.4,47.5,72.4,28.8,79.2C10.1,86,-10.8,86.7,-29.3,80.1C-47.7,73.5,-63.7,59.7,-73.6,42.5C-83.5,25.3,-87.4,4.6,-84.2,-15.1C-81.1,-34.8,-70.9,-53.6,-55.7,-66.4C-40.4,-79.2,-20.2,-86.1,-0.5,-85.4C19.2,-84.8,38.4,-76.7,53.2,-63.9Z"
+                        transform="translate(100 100)"
+                    />
+                </svg>
+            </span>
         </div>
         </div>
     );
