fff632a466879e0b604f714c7792477d9e8ea1f4
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-08-06 08:26:28 +0700

    fix link of dataset and show on the web

--- src/data.js
+++ src/data.js
@@ -1,90 +1,97 @@
+import GitHub from "./icons/GitHub";
+import LinkedIn from "./icons/LinkedIn";
+import Instagram from "./icons/Instagram";
+import TikTok from "./icons/TikTok";
+
 export const DataTools = [
-    {
-        id: 1,
-        toolName: "HTML",
-        image: '/public/tools/html5-original.svg'
-    },
-    {
-        id: 2,
-        toolName: "CSS",
-        image: '/public/tools/css3-original.svg'
-    },
-    {
-        id: 3,
-        toolName: "JavaScript",
-        image: '/public/tools/javascript-original.svg'
-    },
-    {
-        id: 4,
-        toolName: "Python",
-        image: '/public/tools/python-original.svg'
-    },
-    {
-        id: 5,
-        toolName: "TailwindCSS",
-        image: '/public/tools/tailwindcss-original.svg'
-    },
-    {
-        id: 6,
-        toolName: "PHP",
-        image: '/public/tools/php-original.svg'
-    },
-    {
-        id: 7,
-        toolName: "Laravel",
-        image: '/public/tools/laravel-original.svg'
-    },
-    {
-        id: 8,
-        toolName: "React",
-        image: '/public/tools/react-original.svg'
-    },
-    {
-        id: 9,
-        toolName: "Git",
-        image: '/public/tools/git-original.svg'
-    },
-]
+  {
+    id: 1,
+    toolName: "HTML",
+    image: "/public/tools/html5-original.svg",
+  },
+  {
+    id: 2,
+    toolName: "CSS",
+    image: "/public/tools/css3-original.svg",
+  },
+  {
+    id: 3,
+    toolName: "JavaScript",
+    image: "/public/tools/javascript-original.svg",
+  },
+  {
+    id: 4,
+    toolName: "Python",
+    image: "/public/tools/python-original.svg",
+  },
+  {
+    id: 5,
+    toolName: "TailwindCSS",
+    image: "/public/tools/tailwindcss-original.svg",
+  },
+  {
+    id: 6,
+    toolName: "PHP",
+    image: "/public/tools/php-original.svg",
+  },
+  {
+    id: 7,
+    toolName: "Laravel",
+    image: "/public/tools/laravel-original.svg",
+  },
+  {
+    id: 8,
+    toolName: "React",
+    image: "/public/tools/react-original.svg",
+  },
+  {
+    id: 9,
+    toolName: "Git",
+    image: "/public/tools/git-original.svg",
+  },
+];
 
 export const ProjectList = [
-    {
-        id: 1,
-        image: '/public/img/projects/project-1.png',
-        name: 'Sumber Manis Product Catalog',
-        description: 'Product catalog website for SMEs developed using the Laravel framework',
-        link: 'https://github.com/ramadhanzakki/katalog-sumbermanis'
-    },
-    {
-        id: 2,
-        image: '/public/img/projects/project-2.png',
-        name: 'PPM Profile',
-        description: 'Website for finding the dormitory (PPM) closest to the university ',
-        link: 'https://ppm.generus.app'
-    },
-    {
-        id: 3,
-        image: '/public/img/projects/project-3.png',
-        name: 'Personal Portfolio Website',
-        description: 'Personal portfolio website created to learn ReactJS',
-        link: 'https://github.com/ramadhanzakki/katalog-sumbermanis'
-    },
-]
+  {
+    id: 1,
+    image: "/public/img/projects/project-1.png",
+    name: "Sumber Manis Product Catalog",
+    description:
+      "Product catalog website for SMEs developed using the Laravel framework",
+    link: "https://github.com/ramadhanzakki/katalog-sumbermanis",
+  },
+  {
+    id: 2,
+    image: "/public/img/projects/project-2.png",
+    name: "PPM Profile",
+    description:
+      "Website for finding the dormitory (PPM) closest to the university ",
+    link: "https://ppm.generus.app",
+  },
+  {
+    id: 3,
+    image: "/public/img/projects/project-3.png",
+    name: "Personal Portfolio Website",
+    description: "Personal portfolio website created to learn ReactJS",
+    link: "https://github.com/ramadhanzakki/katalog-sumbermanis",
+  },
+];
 
 export const SocialMediaList = [
-    {
-        svg: './icons/GitHub.jsx',
-        link: 'https://github.com/ramadhanzakki',
-    },
-    {
-        svg: './icons/LinkedIn.jsx',
-        link: 'https://github.com/ramadhanzakki',
-    },
-    {
-        svg: './icons/Instagram.jsx',
-        link: 'https://www.instagram.com/rmdhnzakki__/',
-    },
-    {
-        svg: './icons/TikTok.jsx',
-        link: 'https://www.tiktok.com/@ramadhanzakki21_',
-    },
-]
+  {
+    icon: GitHub,
+    link: "https://github.com/ramadhanzakki",
+  },
+  {
+    icon: LinkedIn,
+    link: "https://github.com/ramadhanzakki",
+  },
+  {
+    icon: Instagram,
+    link: "https://www.instagram.com/rmdhnzakki__/",
+  },
+  {
+    icon: TikTok,
+    link: "https://www.tiktok.com/@ramadhanzakki21_",
+  },
+];
--- src/layout/Contact.jsx
+++ src/layout/Contact.jsx
@@ -1,26 +1,45 @@
+import { SocialMediaList } from "../data";
+
 const Contact = () => {
     return (
         <div>
             <div>
-                <h3 className="text-base font-normal uppercase px-3 py-1 text-shadow-cozy-bg bg-brand-cinnamon max-w-fit rounded-lg mb-2">COLLABORATION</h3>
-                <h2 className="text-2xl/snug font-semibold text-cozy-text w-2/3">Have a vision?</h2>
-                <p className="text-base/loose font-normal text-cozy-muted w-4/5">Open to new ventures and creative opportunities. Let’s craft something brilliant together.</p>
+                <h3 className="text-base font-normal uppercase px-3 py-1 text-shadow-cozy-bg bg-brand-cinnamon max-w-fit rounded-lg mb-2">
+                    COLLABORATION
+                </h3>
+                <h2 className="text-2xl/snug font-semibold text-cozy-text w-2/3">
+                    Have a vision?
+                </h2>
+                <p className="text-base/loose font-normal text-cozy-muted w-4/5">
+                    Open to new ventures and creative opportunities. Let’s craft something
+                    brilliant together.
+                </p>
                 <button>Connect Now</button>
             </div>
             <div>
-                <h3 className="text-base font-normal uppercase px-3 py-1 text-shadow-cozy-bg bg-brand-cinnamon max-w-fit rounded-lg mb-2">follow me</h3>
+                <h3 className="text-base font-normal uppercase px-3 py-1 text-shadow-cozy-bg bg-brand-cinnamon max-w-fit rounded-lg mb-2">
+                    follow me
+                </h3>
                 <div>
-                    <a 
-                        href="https://github.com/ramadhanzakki" 
-                        target="_blank" 
-                        className="text-cozy-text hover:text-brand-amber transition ease-linear duration-75"
+                {SocialMediaList.map((social, index) => {
+                    const Icon = social.icon;
+
+                    return (
+                    <a
+                        href={social.link}
+                        key={index}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-cozy-text hover:text-brand-amber transition ease-linear duration-75 inline-flex items-center justify-center p-2"
                     >
-                        <svg role="img" viewBox="0 0 24 24" width={40} className="fill-current" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+                        <Icon />
                     </a>
+                    );
+                })}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
